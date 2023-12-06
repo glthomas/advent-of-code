e7ce938dfe7081718fb0ba92a9377d5b10fc37eb
jerry thomas <geraldleethomas@hotmail.com>
2023-12-06 00:05:38 -0600

    concise

--- src/2023/days/day06.js
+++ src/2023/days/day06.js
@@ -1,11 +1,6 @@
-import {getInput} from "../../getinput.js";
-import _ from "lodash";
-
 export async function day(day) {
-   
     stars(11)
     stars(12)
-    
 }
 
 function productWays(times,records){
@@ -16,10 +11,7 @@ function productWays(times,records){
 }
 
 export function stars(star){
-
     let times = star == 11 ? [57,72,69,92] : [57726992]
     let records = star == 11 ? [291,1172,1176,2026] : [291117211762026]
-    let product = 1
-
     console.log(`star${star}: ${productWays(times,records)}`)
 }
